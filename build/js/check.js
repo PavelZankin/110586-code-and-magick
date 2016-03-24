42b3144a25d7e94b7583f13ca16cede7571ee81d
Pavel Zankin <Littlemooch73@mail.ru>
2016-03-24 20:04:44 +0300

    module1-task2
    correcting

--- build/js/check.js
+++ build/js/check.js
@@ -1,28 +1,36 @@
+function sum(array){
+	var arraySum = 0;
+    for(var i = 0; i < array.length; i++){
+        arraySum += array[i];
+        }
+    	return arraySum;
+}
+
+function length(array1, array2){
+	var arrayProd = 0;
+	for(var i = 0; i < array1.length && i < array2.length; i++) {
+			arrayProd += array1[i] * array2[i];
+		}
+		return arrayProd;	
+}
+
 function getMessage(a, b) {
 	
 	if (typeof a == "boolean") {
 		if (a) {
-			return "я попал в [" + b + "]";
+			return "Я попал в [" + b + "]";
 		}
 		else {
-			return "я никуда не попал";
+			return "Я никуда не попал";
 		}
 	}
 	 else if (typeof a == "number") {
 		return "Я прыгнул на [" + a * 100 + "] сантиметров";
 	}
 	 else if (typeof a == "object" && typeof b !== "object") {
-		var sum = 0;
-		for(var i = 0; i < a.length; i++) {
-			sum += a[i];
-		}
-		return "Я прошёл [" + sum + "] шагов";
+		return "Я прошёл [" + sum(a) + "] шагов";
 	} 
-	else if (typeof a == "object" && typeof b == "object") {
-		var length = 0;
-		for(var i = 0; i < a.length && i < b.length; i++) {
-			length += a[i] * b[i];
-		}		
-		return "Я прошёл [" + length +"] метров";
+	else if (typeof a == "object" && typeof b == "object") {		
+		return "Я прошёл [" + length(a, b) +"] метров";
 	}
 }
